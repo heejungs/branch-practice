f0762758a55fbdbba2e0132bf079ceb59cb1fd73
heejungs <hee980313@gmail.com>
2021-01-29 12:43:34 +0900

    feat:Add elif clause

--- hello.py
+++ hello.py
@@ -3,6 +3,8 @@ def print_hello():
     for i in range(1,num+1):
         if i%2 == 0:
             print('hello git for {}th time(s)'.format(i))
+        elif i%3 ==0:
+            print('oh {} is odd but, hello'.format(i))
         else:
             print('nope, i is odd')
 
